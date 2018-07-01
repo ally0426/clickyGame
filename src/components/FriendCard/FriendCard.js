f883ed16491b7bfd89fee5b204d414a7a9cfa272
Youngmi Lee <abinding@gmail.com>
2018-07-01 12:49:14 -0500

    editied component files

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -2,12 +2,11 @@
 import React from "react";
 import "./FriendCard.css";
 
+import React from "react";
+import "./FriendCard.css";
+
 const FriendCard = props => (
-  <div 
-    className="card" 
-    value={props.id} 
-    onClick={() => props.handleClick(props.id)}
-  >
+  <div className="card" value={props.id} onClick={() => props.handleClick(props.id)}>
     <div className="img-container">
       <img alt={props.name} src={props.image} />
     </div>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,14 +4,9 @@ import "./Nav.css";
 const Nav = props => (
   <nav>
     <ul>
-      <li className="brand animated lightSpeedIn alignLeft">
-        <a href="/clicky-game/">{props.title}</a>
-      </li>
-      <li id="rw" >{props.correctIncorrect}</li>
-
-
-      <li className="alignRight">Score - Top: {props.topScore} | Current: {props.score}</li>
-
+     <li className="brand animated lightSpeedIn alignLeft"><a href="/clicky-game/">{props.title}</a></li>
+     <li id="rw">{props.correctIncorrect}</li>
+     <li className="alignRight">Score - Top: {props.topScore} | Current: {props.score}</li>
     </ul>
   </nav>
 );
