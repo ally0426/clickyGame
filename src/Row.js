b5e0be7bcd1abf5631731350dd6cfbdeeeba93ed
Youngmi Lee <abinding@gmail.com>
2018-07-01 13:28:28 -0500

    edited Row js file

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,8 @@
 import React from "react";
 
-const Row = props => {
+const Row = props => 
   <div className={`row${props.fluid ? "-fluid" : ""}`}>
     {props.children}
   </div>;
-}
 
 export default Row;
