711b232b5872c0b1e480541b765bea61388642be
Youngmi Lee <abinding@gmail.com>
2018-07-01 13:01:27 -0500

    editied FriendCard compoment

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -2,9 +2,6 @@
 import React from "react";
 import "./FriendCard.css";
 
-import React from "react";
-import "./FriendCard.css";
-
 const FriendCard = props => (
   <div className="card" value={props.id} onClick={() => props.handleClick(props.id)}>
     <div className="img-container">
